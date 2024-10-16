935d7b4c93437ac1210046df29d24c6a28f013df
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-10-16 09:54:13 +0700

    update: add missing candidate layout

---
+++ resources/views/layouts/candidate.blade.php
@@ -0,0 +1,26 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebPilKetos | @yield('title')</title>
+</head>
+
+<body>
+    <x-alert />
+    <div>
+        <header>
+            <h1>@yield('header')</h1>
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            @yield('footer')
+        </footer>
+    </div>
+</body>
+
+</html>
