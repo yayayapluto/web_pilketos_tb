4a7d7af3f8278784c417d727d975d5cc12e6eb34
bluebanana21 <m.affanfahrozi@gmail.com>
2024-10-16 19:05:27 +0700

    update: back button

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -7,13 +7,26 @@
 @endsection
 
 @section('content')
+    <style>
+        svg:hover {
+            fill: rgb(222, 222, 222);
+        }
+    </style>
+
     <div class="container-fluid"
         style="background-image: url('../../assets/logoTB/Screenshot 2024-10-16 135500.png'); padding-bottom: 50px; background-repeat: no-repeat;
 background-size: contain;">
         <div class="container">
-            <div>
-                <div class="col-1">
-                    <button type="button" class="btn-close mt-3" aria-label="Close"></button>
+            <div class="back-button">
+                <div style="padding-top:50px;">
+                    <a href="{{ route('voting') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
+                            class="bi bi-x-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                            <path
+                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
@@ -21,7 +34,8 @@ background-size: contain;">
         <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
             <div class="row bg-white shadow-lg rounded p-4" style="max-width: 1100px;">
                 <div class="col-md-5 text-center">
-                    <img style="height:300px;width:300px;object-fit:cover;" src="{{ asset('storage/' . $candidate->image) }}" class="rounded img-fluid mb-3"
+                    <img style="height:300px;width:300px;object-fit:cover;"
+                        src="{{ asset('storage/' . $candidate->image) }}" class="rounded img-fluid mb-3"
                         alt="Profile Image">
                     <p><strong>NISN:</strong> 17813719</p>
                 </div>
