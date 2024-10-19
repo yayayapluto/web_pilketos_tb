c8ec0ce715188ad159d0b4a576028e0dcca3cc42
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-10-19 13:55:08 +0700

    update: candidate detail redircet back

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -19,7 +19,7 @@ background-size: contain;">
         <div class="container">
             <div class="back-button">
                 <div style="padding-top:50px;">
-                    <a href="{{ route('voting') }}">
+                    <a href="{{ URL::previous() }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white"
                             class="bi bi-x-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
